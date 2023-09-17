f0bceeefec0838240de6a6c8f5ca370970764c39
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2023-09-17 14:18:14 +0530

    Create counter.jsx

---
+++ Offline Classes/counter/counter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const counter = () => {
+    const [ count ,setCount] = useState(0);
+    
+  return (
+    <div>
+    <button></button>
+
+
+
+    counter</div>
+  )
+}
+
+export default counter
