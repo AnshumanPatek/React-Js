8533df6ae5f5102514745e13fa3b4e46541333f0
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2023-09-17 13:54:23 +0530

    Add files via upload

---
+++ Offline Classes/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+//import ChangeName from "./Components/check";
+import counter from "./counter/counter";
+
+function App() {
+  return(
+  <div className="contianer">
+    <counter/>
+  </div>
+  
+  )
+}
+
+export default App;
