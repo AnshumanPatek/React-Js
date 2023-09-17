a22d9dbc314b83a82001ebb8b5ed5ed0a8ec7081
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2023-09-17 13:57:56 +0530

    Add files via upload

---
+++ Offline Classes/Components/check.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ChangeName = () => {
+  let name = "Anshuman Patek";
+  // let name2 ="Anshuman";
+  alert("this is alert");
+  return (
+    <div>
+    hello
+      <h2> Your Name is {name}</h2>
+      <button>ChangeName</button>
+    </div>
+  );
+};
+export default ChangeName;
